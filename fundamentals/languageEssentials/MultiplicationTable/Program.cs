ea492088198c8d30fad23d04c8b3bb89b17b5f10
Luis Endres <luisendres@yahoo.com>
2021-08-04 10:44:14 -0700

    UPDATED CODE FOR PROBLEM

--- fundamentals/languageEssentials/MultiplicationTable/Program.cs
+++ fundamentals/languageEssentials/MultiplicationTable/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace MultiplicationTable
 {
@@ -6,15 +7,51 @@ namespace MultiplicationTable
     {
         static void Main(string[] args)
         {
-            int[,] multiplicationTable = new int[10,10]
-            {
-                {1,2,3,4,5,6,7,8,9,10}, {2,4,6,8,10,12,14,16,18,20}, {3,6,9,12,15,18,21,24,27,30}, {4,8,12,16,20,24,28,32,36,40}, {5,10,15,20,25,30,35,40,45,50},
-                {6,12,18,24,30,36,42,48,54,60}, {7,15,21,28,35,42,49,56,63,70}, {8,16,24,32,40,48,56,64,72,80}, {9,18,27,36,45,54,63,72,81,90}, {10,20,30,40,50,60,70,80,90,100}
-            };
+            //UPDATED CODE FOR PROBLEM 
+            Console.WriteLine("Hello World!");
+            var table = BuildTable(10);
+            RenderTable(table);
 
-            foreach (int table in multiplicationTable) {
-                Console.WriteLine(table);
+            // ORIGINAL CODE FOR PROBLEM 
+            // int[,] multiplicationTable = new int[10,10]
+            // {
+            //     {1,2,3,4,5,6,7,8,9,10}, {2,4,6,8,10,12,14,16,18,20}, {3,6,9,12,15,18,21,24,27,30}, {4,8,12,16,20,24,28,32,36,40}, {5,10,15,20,25,30,35,40,45,50},
+            //     {6,12,18,24,30,36,42,48,54,60}, {7,15,21,28,35,42,49,56,63,70}, {8,16,24,32,40,48,56,64,72,80}, {9,18,27,36,45,54,63,72,81,90}, {10,20,30,40,50,60,70,80,90,100}
+            // };
+
+            // foreach (int row in multiplicationTable) {
+            //     Console.WriteLine(row);
+            // }
+        }
+
+        //UPDATED CODE FOR PROBLEM 
+        public static int[,] BuildTable(int size)
+        {
+            int[,] table = new int[size,size];
+            for(var row = 0; row < size; row++)
+                for(var col = 0; col < size; col++)
+                    table[row,col] = (row + 1) * (col + 1);
+            return table;
+        }
+        public static void RenderTable(int[,] multiArray)
+        {
+            //THIS NEEDS "using System.Text;" TO WORK 
+            // StringBuilder class good for mutating strings
+            StringBuilder sb = new StringBuilder();
+
+            // gets length of specified dimention of multi array
+            int size = multiArray.GetLength(0);
+            
+            for(var row = 0; row < size; row++)
+            {
+                sb.Append("[");
+                for(var col = 0; col < size; col++)
+                {
+                    sb.Append($"{multiArray[row,col]}, ");
+                }
+                sb.Append("]\n");
             }
+            Console.WriteLine(sb);
         }
     }
 }
