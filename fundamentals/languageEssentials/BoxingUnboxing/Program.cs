fa96b43144788cd0bce1179852dc8a125982f55e
Luis Endres <luisendres@yahoo.com>
2021-08-04 11:13:13 -0700

    intial commit finished

---
+++ fundamentals/languageEssentials/BoxingUnboxing/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> weird = new List<object>();
+            weird.Add(7);
+            weird.Add(28);
+            weird.Add(-1);
+            weird.Add(true);
+            weird.Add("chair");
+
+            int sum = 0;
+            foreach (object see in weird) {
+                if (see is string) 
+                {
+                    Console.WriteLine("string");
+                }
+                if (see is int) 
+                {
+                    Console.WriteLine("int");
+                    sum += (int)see;
+                }
+                if (see is bool) 
+                {
+                    Console.WriteLine("BOOLEAN");
+                }
+                Console.WriteLine(see);
+            }
+                Console.WriteLine(sum);
+        }
+    }
+}
