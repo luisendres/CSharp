072b897688ba0a8daee89ac94004477f18b73d59
Luis Endres <luisendres@yahoo.com>
2021-08-04 11:24:40 -0700

    intial commit setup for Basic 13

---
+++ fundamentals/languageEssentials/Basic13/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Basic13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print 1-255
+
+            // Print odd numbers between 1-255
+
+            // Iterating through an Array
+
+            // Print Sum
+            
+            // Find Max
+
+            // Get Average
+
+            // Array with Odd Numbers
+
+            // Greater than Y
+
+            // Square the Values
+
+            // Eliminate Negative Numbers
+
+            // Min, Max, and Average
+
+            // Shifting the values in an array
+
+            // Number to String
+        }
+    }
+}
