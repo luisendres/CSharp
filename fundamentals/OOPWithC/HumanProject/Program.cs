582f33f9b02e1cf90ff1090b77160487568e2062
Luis Endres <luisendres@yahoo.com>
2021-08-04 21:57:00 -0700

    intital commit replace old file

---
+++ fundamentals/OOPWithC/HumanProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HumanProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
