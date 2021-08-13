98e35db2f3b49954211ff6bc7e295b752128b615
Luis Endres <luisendres@yahoo.com>
2021-08-13 15:59:11 -0700

    initial Commit and Finished site

---
+++ ORMs/LogAndReg/Controllers/HomeController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using LogAndReg.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
+
+namespace LogAndReg.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private LogAndRegContext db;
+        public HomeController(LogAndRegContext context)
+        {
+            db = context;
+        }
+
+        private int? uid
+        {
+            get
+            {
+                return HttpContext.Session.GetInt32("UserId");
+            }
+        }
+
+        private bool isLoggedIn
+        {
+            get
+            {
+                return uid != null;
+            }
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpGet("/login")]
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+
+        [HttpGet("/success")]
+        public IActionResult Success()
+        {
+            if (!isLoggedIn)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View("Success");
+        }
+
+        [HttpPost("/register")]
+        public IActionResult Register(User newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                // If email already exists.
+                if (db.Users.Any(u => u.Email == newUser.Email))
+                {
+                    ModelState.AddModelError("Email", "is taken.");
+                }
+                /* We could add more custom errors */
+                // if (newUser.FirstName == "Lawrence")
+                // {
+                //     ModelState.AddModelError("FirstName", "Sorry, no Lawrences allowed.");
+                // }
+            }
+
+            // If any above custom errors were added, ModelState would now be invalid.
+            if (ModelState.IsValid == false)
+            {
+                // So error messages are displayed.
+                return View("Index");
+            }
+
+            PasswordHasher<User> hasher = new PasswordHasher<User>();
+            newUser.Password = hasher.HashPassword(newUser, newUser.Password);
+
+            db.Users.Add(newUser);
+            db.SaveChanges();
+
+            HttpContext.Session.SetInt32("UserId", newUser.UserId);
+            HttpContext.Session.SetString("FirstName", newUser.FirstName);
+            return RedirectToAction("Success");
+        }
+
+        [HttpPost("/login")]
+        public IActionResult Login(LoginUser loginUser)
+        {
+            if (ModelState.IsValid == false)
+            {
+                // Display error messages.
+                return View("Login");
+            }
+
+            User dbUser = db.Users.FirstOrDefault(user => user.Email == loginUser.LoginEmail);
+
+            if (dbUser == null)
+            {
+                /*
+                Usually you don't want to reveal specifics, like was the email
+                or password invalid because of phishing. But for testing it's
+                easier to see what actually went wrong with specific errors.
+                */
+                ModelState.AddModelError("LoginEmail", "Email not found.");
+                return View("Login");
+            }
+
+            PasswordHasher<LoginUser> hasher = new PasswordHasher<LoginUser>();
+            PasswordVerificationResult pwCompareResult = hasher.VerifyHashedPassword(loginUser, dbUser.Password, loginUser.LoginPassword);
+
+            if (pwCompareResult == 0)
+            {
+                // Don't reveal this info, this is just for testing.
+                ModelState.AddModelError("LoginPassword", "Invalid Password");
+                return View("Login");
+            }
+
+            HttpContext.Session.SetInt32("UserId", dbUser.UserId);
+            HttpContext.Session.SetString("FirstName", dbUser.FirstName);
+            return RedirectToAction("Success");
+        }
+
+        [HttpPost("/logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ORMs/LogAndReg/Models/LogAndRegContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace LogAndReg.Models
+{
+    public class LogAndRegContext : DbContext
+    {
+        public LogAndRegContext(DbContextOptions options) : base(options) { }
+
+        // for every model / entity that is going to be part of the db
+        // the names of these properties will be the names of the tables in the db
+        public DbSet<User> Users { get; set; }
+
+        // public DbSet<Widget> Widgets { get; set; }
+        // public DbSet<Item> Items { get; set; }
+    }
+}
